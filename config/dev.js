2188731ace0172b9964a2d50acb783f656b088f7
developer-Park <83831110+developer-Park@users.noreply.github.com>
2022-01-13 22:08:19 -0800

    Delete dev.js

--- config/dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb://dudaksdk3214:cjstk5068@boilerplate-shard-00-00.d4uud.mongodb.net:27017,boilerplate-shard-00-01.d4uud.mongodb.net:27017,boilerplate-shard-00-02.d4uud.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-7i54lw-shard-0&authSource=admin&retryWrites=true&w=majority"
-}
-
